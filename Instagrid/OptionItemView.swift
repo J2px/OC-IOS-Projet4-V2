12e4ad3c2bc88f70d8871557e859b2275ddd28a6
J2PX <jimmy.jermidi@gmail.com>
2023-11-17 11:50:22 +0100

    commit 17-11-23

--- Instagrid/OptionItemView.swift
+++ Instagrid/OptionItemView.swift
@@ -8,22 +8,22 @@
 import UIKit
 
 class OptionItemView: UIView {
-
+    
     private var choiceImg1 : UIImageView!
     private var choiceImg2 : UIImageView!
     private var choiceImg3 : UIImageView!
     private var selectedView : UIImageView!
     
     
-    enum Choice {
+    enum Choice: CaseIterable {
         case choice1, choice2, choice3
     }
     
-    /*var choice: Choice = .choice1 {
+    var choice: Choice = .choice1 {
         didSet {
-            setChoice(choice)
+            setChoices(choice)
         }
-    }*/
+    }
     
     
     
@@ -35,15 +35,52 @@ class OptionItemView: UIView {
         fatalError("init(coder:) has not been implemented")
     }
     
-   /* private func setChoice(_ choice: Choice){
+    private func setChoices(_ choice: Choice){
         switch choice {
-        case .choice1:
-        case .choice2:
-        case .choice3:
-        }
+        case .choice1: 
+    choiceImg1.image = UIImage(named: "Layout_1")
+            initOptionImageView(choice: choiceImg1)
+    initSelectedImageView()
+    case .choice2:
+    choiceImg2.image = UIImage(named: "Layout_2")
+            initOptionImageView(choice: choiceImg2)
+    initSelectedImageView()
+    case .choice3:
+    choiceImg3.image = UIImage(named: "Layout_3")
+            initOptionImageView(choice: choiceImg3)
+    initSelectedImageView()
+    }
         
-    }*/
+    }
     
+    private func initOptionImageView(choice: UIImageView) {
+        choice.contentMode = .scaleToFill
+        choice.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(choice)
+        NSLayoutConstraint.activate([
+            choice.topAnchor.constraint(equalTo: topAnchor, constant: 0),
+            choice.bottomAnchor.constraint(equalTo: bottomAnchor, constant: 0),
+            choice.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 0),
+            choice.trailingAnchor.constraint(equalTo: trailingAnchor, constant: 0)
+        ])
+    }
+    
+    
+    private func initSelectedImageView() {
+        selectedView = UIImageView()
+        selectedView.contentMode = .scaleToFill
+        selectedView.translatesAutoresizingMaskIntoConstraints = false
+        selectedView.image = UIImage(named: "Selected")
+        addSubview(selectedView)
+        NSLayoutConstraint.activate([
+            selectedView.topAnchor.constraint(equalTo: topAnchor, constant: 0),
+            selectedView.bottomAnchor.constraint(equalTo: bottomAnchor, constant: 0),
+            selectedView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 0),
+            selectedView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: 0)
+        ])
+        isSelected(selectedView: selectedView)
+    }
+        
     private func isSelected(selectedView : UIImageView) {
         selectedView.isHidden = false
     }
--- Instagrid/ViewController.swift
+++ Instagrid/ViewController.swift
@@ -19,45 +19,91 @@ class ViewController: UIViewController,UIImagePickerControllerDelegate, UINaviga
     @IBOutlet weak var ImgButton4: UIButton!
     
     
+    @IBOutlet weak var stackView: UIStackView!
     
     
     
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        controlGesture(button: ImgButton1)
+        controlGesture(button: ImgButton2)
+        controlGesture(button: ImgButton3)
+        controlGesture(button: ImgButton4)
+        controlChoices()
         
-        let tapGesture1 = UITapGestureRecognizer(target: self, action: #selector(imageButtonTapped(_:)))
-        ImgButton1.addGestureRecognizer(tapGesture1)
-
-                let tapGesture2 = UITapGestureRecognizer(target: self, action: #selector(imageButtonTapped(_:)))
-        ImgButton2.addGestureRecognizer(tapGesture2)
-
-                let tapGesture3 = UITapGestureRecognizer(target: self, action: #selector(imageButtonTapped(_:)))
-        ImgButton3.addGestureRecognizer(tapGesture3)
-
-                let tapGesture4 = UITapGestureRecognizer(target: self, action: #selector(imageButtonTapped(_:)))
-        ImgButton4.addGestureRecognizer(tapGesture4)
-           }
+        
+        
+    }
     
     
     @objc func imageButtonTapped(_ sender: UITapGestureRecognizer) {
-            selectedButton = sender.view as? UIButton
-            let imagePicker = UIImagePickerController()
-            imagePicker.sourceType = .photoLibrary
-            imagePicker.delegate = self
-            imagePicker.allowsEditing = true
-            present(imagePicker, animated: true, completion: nil)
-        }
+        selectedButton = sender.view as? UIButton
+        let imagePicker = UIImagePickerController()
+        imagePicker.sourceType = .photoLibrary
+        imagePicker.delegate = self
+        imagePicker.allowsEditing = true
+        present(imagePicker, animated: true, completion: nil)
+    }
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
         if let originalImage = info[.editedImage] as? UIImage, let button = selectedButton {
             button.setImage(originalImage, for: .normal)
             /*if let croppedImage = resizeAndCropImage(originalImage, toSize: button.bounds.size) {
-                
-            }*/
+             
+             }*/
         }
         picker.dismiss(animated: true, completion: nil)
     }
+    
+    private func controlGesture(button : UIButton){
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(imageButtonTapped(_:)))
+        button.addGestureRecognizer(tapGesture)
+    }
+    
+    
+    private func controlChoices(){
+        let allLayouts = OptionItemView.Choice.allCases
+        var allOptions = [createOption()]
+        
+        // Append Option in stackView
+        for optionLayout in allLayouts {
+            let option = createOption()
+            option.choice = optionLayout
+            self.stackView.addArrangedSubview(option)
+            allOptions.append(option)
+            
+            
+        }
+    }
+    
+    
+    private func createOption() -> OptionItemView {
+        let option = OptionItemView(frame: CGRect.zero)
+        option.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            option.widthAnchor.constraint(equalToConstant: 80),
+            option.heightAnchor.constraint(equalToConstant: 80)
+        ])
+        return option
+    }
+    
+    private func makeChoice(choice: OptionItemView.Choice){
+        switch choice{
+        case .choice1:
+            ImgButton2.isHidden = true
+            
+        case .choice2:
+            ImgButton4.isHidden = true
+        case .choice3:
+            ImgButton2.isHidden = false
+            ImgButton4.isHidden = false
+        }
+        
+    }
+    
+    
+    
 }
 
