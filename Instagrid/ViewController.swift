4203407180a3d926659042b0c3208da16ea8d0cb
aliabbas90 <ali@abbas.net>
2023-12-08 12:33:57 +0100

    Update fonts and add marks

--- Instagrid/ViewController.swift
+++ Instagrid/ViewController.swift
@@ -11,72 +11,37 @@ import UIKit
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate{
     
-    let imagePicker = UIImagePickerController()
-    var selectedButton: UIButton?
+    // MARK: - Private vars
+    private let imagePicker = UIImagePickerController()
+    private var selectedButton: UIButton?
+    private var allOptionsView: [OptionItemView] = []
     
-    
-    @IBOutlet weak var ImgButton1: UIButton!
-    @IBOutlet weak var ImgButton2: UIButton!
-    @IBOutlet weak var ImgButton3: UIButton!
-    @IBOutlet weak var ImgButton4: UIButton!
-    
+    // MARK: - IBOutlets
+    @IBOutlet weak var imgButton1: UIButton!
+    @IBOutlet weak var imgButton2: UIButton!
+    @IBOutlet weak var imgButton3: UIButton!
+    @IBOutlet weak var imgButton4: UIButton!
     @IBOutlet weak var titleLabel: UILabel!{
         didSet {
             titleLabel.font = UIFont(name: "ThirstySoftRegular", size: 32)
         }
     }
-        
-        
     @IBOutlet weak var swipeLabel: UILabel!{
         didSet {
             swipeLabel.font = UIFont(name: "Delm-Medium", size: 25)
         }
     }
-    
     @IBOutlet var mainView: UIView!
-    
     @IBOutlet weak var stackView: UIStackView!
     
     
-    
-    
+    // MARK: - Life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        
-        
         setupChoices()
         NotificationCenter.default.addObserver(self, selector: #selector(setUpSwipe), name: UIDevice.orientationDidChangeNotification, object: nil)
-
-        
         imagePicker.delegate = self
-        
-        
     }
-    
-    
-    func showImagePicker(with sourceType: UIImagePickerController.SourceType) {
-            if UIImagePickerController.isSourceTypeAvailable(sourceType) {
-                imagePicker.sourceType = .photoLibrary
-                imagePicker.delegate = self
-                imagePicker.allowsEditing = true
-                present(imagePicker, animated: true, completion: nil)
-            } else {
-                print("La source spécifiée n'est pas disponible.")
-            }
-        }
-    
-    
-    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
-        if let originalImage = info[.editedImage] as? UIImage, let button = selectedButton {
-            button.setImage(originalImage, for: .normal)
-        }
-        picker.dismiss(animated: true, completion: nil)
-    }
-    
-    
-    
-    private var allOptionsView: [OptionItemView] = []
     
     private func setupChoices(){
         let allLayouts = OptionItemView.Choices.allCases
@@ -89,14 +54,12 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         }
     }
     
-    
     private func createOptionView(choice: OptionItemView.Choices) -> OptionItemView {
         let option = OptionItemView(choice: choice, frame: CGRect.zero)
         option.translatesAutoresizingMaskIntoConstraints = false
         option.onTapAction = {
-            self.deselectAll()
+            self.deselectAllOptions()
             option.isSelected = true
-            // TODO: Changer le format de la vue centrale 
             self.makeChoice(choice: choice)
         }
         NSLayoutConstraint.activate([
@@ -106,7 +69,7 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         return option
     }
     
-    private func deselectAll() {
+    private func deselectAllOptions() {
         allOptionsView.forEach { option in
             option.deselectOption()
         }
@@ -115,62 +78,53 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     private func makeChoice(choice: OptionItemView.Choices){
         switch choice{
         case .choice1:
-            ImgButton1.isHidden = false
-            ImgButton2.isHidden = true
-            ImgButton3.isHidden = false
-            ImgButton4.isHidden = false
-            
+            imgButton1.isHidden = false
+            imgButton2.isHidden = true
+            imgButton3.isHidden = false
+            imgButton4.isHidden = false
         case .choice2:
-            ImgButton1.isHidden = false
-            ImgButton2.isHidden = false
-            ImgButton3.isHidden = false
-            ImgButton4.isHidden = true
+            imgButton1.isHidden = false
+            imgButton2.isHidden = false
+            imgButton3.isHidden = false
+            imgButton4.isHidden = true
         case .choice3:
-            ImgButton1.isHidden = false
-            ImgButton2.isHidden = false
-            ImgButton3.isHidden = false
-            ImgButton4.isHidden = false
+            imgButton1.isHidden = false
+            imgButton2.isHidden = false
+            imgButton3.isHidden = false
+            imgButton4.isHidden = false
         }
-        
     }
     
-    
+    // MARK: - IBActions
     @IBAction func buttonAction1(_ sender: Any) {
-        selectedButton = ImgButton1
+        selectedButton = imgButton1
         showImagePicker(with: .photoLibrary)
     }
-    
     
     @IBAction func buttonAction2(_ sender: Any) {
-        selectedButton = ImgButton2
+        selectedButton = imgButton2
         showImagePicker(with: .photoLibrary)
     }
-    
-    
     
     @IBAction func buttonAction3(_ sender: Any) {
-        selectedButton = ImgButton3
+        selectedButton = imgButton3
         showImagePicker(with: .photoLibrary)
     }
     
-    
     @IBAction func buttonAction4(_ sender: Any) {
-        selectedButton = ImgButton4
+        selectedButton = imgButton4
         showImagePicker(with: .photoLibrary)
     }
     
     @objc func setUpSwipe() {
-            if UIDevice.current.orientation.isPortrait {
-                setRecognizerUp()
-                swipeLabel.text = " Swipe up to share "
-            } else if UIDevice.current.orientation.isLandscape {
-                setRecognizerLeft()
-                swipeLabel.text = " Swipe left to share"
-            }
+        if UIDevice.current.orientation.isPortrait {
+            setRecognizerUp()
+            swipeLabel.text = " Swipe up to share "
+        } else if UIDevice.current.orientation.isLandscape {
+            setRecognizerLeft()
+            swipeLabel.text = " Swipe left to share"
         }
-
-
-    
+    }
     
     private func setRecognizerUp(){
         let swipeGestureRecognizerUp = UISwipeGestureRecognizer(target: self, action: #selector(didSwipe(_:)))
@@ -198,5 +152,25 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         present(activityController, animated: true)
     }
     
+    
+    // MARK: - Picker
+    func showImagePicker(with sourceType: UIImagePickerController.SourceType) {
+        if UIImagePickerController.isSourceTypeAvailable(sourceType) {
+            imagePicker.sourceType = .photoLibrary
+            imagePicker.delegate = self
+            imagePicker.allowsEditing = true
+            present(imagePicker, animated: true, completion: nil)
+        } else {
+            print("La source spécifiée n'est pas disponible.")
+        }
+    }
+    
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
+        if let originalImage = info[.editedImage] as? UIImage, let button = selectedButton {
+            button.setImage(originalImage, for: .normal)
+        }
+        picker.dismiss(animated: true, completion: nil)
+    }
 }
 
